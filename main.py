552332beb1933291a7a97b1706941580b57b03c3
Oleksandr Tertyshnyk <aleksandr.tertyshnyk@gmail.com>
2023-04-17 10:55:50 +0300

    Working on extract title

--- main.py
+++ main.py
@@ -30,19 +30,34 @@ def extruct_title(cars):
     for car in cars:
         #'Продажа Geely Emgrand II, 2023 г. в Минске'
         # type, brand, model, restailing, no, plases, year, location
-        base = car["title"].replace(" · ", "%").replace(".", "%")
-        type = car["title"].split()[0]
-        brand = car["title"].split()[1]
-        model = base.split("%")[0][base.find(brand) + len(brand) + 1:]
-        restailing = "1" # car["title"].split("·")[1]
-        no = "1" #car["title"].split("·")[2].spleat(",")[0]
-        plases = "plases"
-        year = car["title"].split(".")[0][-6:-2]
-        location_type = car["title"].split(".")[0][-1:]
-        location = car["title"].split(".")[1][3:]
+        str = car["title"]
+        type = str.split()[0]
+        brand = str.split()[1]
+        str = str.split(" ", 2)[2]
+
+        year = str.split(".")[0][-6:-2]
+        location_type = str.split(".")[0][-1:]
+        location = str.split(".")[1][3:]
+        str = str.split(".")[0][:-8].replace(" · ", "%").replace(" (", "%").replace("),", "%")
+        column = len(str.split("%"))
+        if column == 1:
+            model = str.split("%")[0]
+            plases = ""
+            restailing = ""
+            no = ""
+        if column == 2:
+            model = str.split("%")[0]
+            plases = ""
+            restailing = str.split("%")[1]
+            no = ""
+        if column == 3:
+            model = str.split("%")[0]
+            plases = ""
+            restailing = str.split("%")[1]
+            no = str.split("%")[2]
         car["title"] = {"type": type, "brand": brand, "model": model, "restailing": restailing, "no": no,
                         "plases": plases, "year": year, "location_type": location_type,  "location": location,
-                        "base": base}
+                        "base": car["title"]}
     return cars
 
 def extruct_description():
@@ -69,8 +84,11 @@ if __name__ == '__main__':
     cars_options = load_cars_otions()
     cars = extruct_title(cars)
     cars = extruct_cars(cars)
-    #print(cars["103779165"])
-    print(cars["104190182"])
+    print(cars["103779165"]["title"])
+    print(cars["104190182"]["title"])
+    print(cars["102596905"]["title"])
+    print(cars["100911865"]["title"])
+
 
     # extruct_cars()
     # extruct_title()
